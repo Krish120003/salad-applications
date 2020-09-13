ce5c285784a3d022f63fd3a6651f502356c96f4a
Krish12003 <39015956+Krish12003@users.noreply.github.com>
2020-09-13 09:13:08 -0400

    Initial repo setup, switch from canary server to dev server for appUrl

--- packages/desktop-app/src/config.ts
+++ packages/desktop-app/src/config.ts
@@ -1,7 +1,7 @@
 class Config {
   // public readonly appUrl: string = 'https://app.salad.io'
-  public readonly appUrl: string = 'https://canary-app.salad.io/'
-  // public readonly appUrl: string = 'http://localhost:3000/'
+  //public readonly appUrl: string = 'https://canary-app.salad.io/'
+  public readonly appUrl: string = 'http://localhost:3000/'
 }
 
 const instance = new Config()
